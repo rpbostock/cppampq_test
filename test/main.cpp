6fa06a4f40fe4743b1a1d6a60f4c19798c25fdea
Richard Bostock <richard.bostock@sportlight.ai>
2025-08-14 13:55:40 +0100

    Updated to not be reliant on gcc13 as this causes issues with the main repo.

--- test/main.cpp
+++ test/main.cpp
@@ -1,10 +1,15 @@
 #include <logging.hpp>
 #include <gtest/gtest.h>
+#include <chrono>
+#include <howardhinnant/date.h>
 
 int main(int argc, char *argv[])
 {
 	// Reducing log output for normal runs
-	Logging::init(true, true, int(boost::log::trivial::info), int(boost::log::trivial::debug), "test_cppamqp.log");
+	auto now = std::chrono::system_clock::now();
+	const auto datetime = date::format("{:%Y%m%d_%H%M%S}", now);
+	std::string log_file_name = "test_cppamqp_" + datetime + ".log";
+	Logging::init(true, true, int(boost::log::trivial::info), int(boost::log::trivial::debug), log_file_name);
 
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
